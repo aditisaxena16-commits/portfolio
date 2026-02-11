ac26edef5de2086b05c9823d6bd7ba582e7de287
aditisaxena16-commits <aditisaxena16@gmail.com>
2026-02-11 07:01:09 +0530

    contact Update

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -10,7 +10,7 @@ export default function Contact() {
                     <div className="grid grid-cols-1 md:grid-cols-2">
                         <div className="p-6 mr-2 bg-[#ded7d7] sm:rounded-lg">
                             <h1 className="text-3xl sm:text-4xl text-gray-800 font-extrabold tracking-tight">
-                                Contact Information
+                                Contact Information Aditi
                             </h1>
                             {/* <p className="text-normal text-lg sm:text-xl font-medium text-gray-600 mt-2">
                                 Fill in the form to start a conversation
